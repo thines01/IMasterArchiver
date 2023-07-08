c95c95a65b95aa184921b5883d0052a2fef13a0d
thines01 <thines01@yahoo.com>
2023-07-08 17:57:28 -0500

    Add project files.

---
+++ IMasterArchiver.cs
@@ -0,0 +1,8 @@
+﻿namespace IMasterArchiver
+{
+   public interface IMasterArchiver<T> where T : class
+   {
+      bool Load(T master, ref string strError);
+      bool Store(T master, ref string strError);
+   }
+}
